0e0369d8d992f844dca594fc21bee4e25ce1494b
anand <anand@DESKTOP-1FBUL6A>
2020-09-09 17:31:21 +0530

    First Commit

---
+++ src/Demo1.java
@@ -0,0 +1,15 @@
+
+public class Demo1 {
+
+	void m1()
+	{
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Demo1 nn = new Demo1();
+  nn.m1();
+	}
+
+}
